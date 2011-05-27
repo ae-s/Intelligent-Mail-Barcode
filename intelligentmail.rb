b5340bbcdbef03aee2b4bbb073d7fbacaff172a3
Astrid Smith <Astrid@xrtc.net>
2011-05-26 21:13:11 -0700

    Now yields codewords, not yet transformed to N-of-13 code.

--- intelligentmail.rb
+++ intelligentmail.rb
@@ -41,15 +41,11 @@ class IMBarcode
       binary += 1000000000 + 100000 + 1
     end
 
-    print binary, "\n"
-
     binary *= 10
     binary += (@code_id / 10)
 
     binary *= 5
     binary += (@code_id % 10)
-
-    print binary, "\n"
 
     for i in 2.downto(0) do
       binary *= 10
@@ -57,7 +53,6 @@ class IMBarcode
     end
 
     if (@mailer_id < 899999)
-      print "short mailer\n"
       # Short mailer ID and long serial number, for high-volume
       # mailers.
       for i in 5.downto(0) do
@@ -69,7 +64,6 @@ class IMBarcode
         binary += (@serial / (10 ** i)) % 10
       end
     else
-      print "long mailer\n"
       # Long mailer ID and short serial number, for low-volume
       # mailers.
       for i in 8.downto(0) do
@@ -83,9 +77,33 @@ class IMBarcode
     end
 
     @binary = binary
-    printf("%X\n", @binary)
+    printf("Encoded bitstring: %X\n", @binary)
 
-    ## TODO translate the CRC code on page 25
+    check = crc(binary, 102)
 
+    binary, code_j = binary.divmod(636)
+    binary, code_i = binary.divmod(1365)
+    binary, code_h = binary.divmod(1365)
+    binary, code_g = binary.divmod(1365)
+    binary, code_f = binary.divmod(1365)
+    binary, code_e = binary.divmod(1365)
+    binary, code_d = binary.divmod(1365)
+    binary, code_c = binary.divmod(1365)
+    code_a, code_b = binary.divmod(1365)
+
+    code_j *= 2
+    if (check & 0x400)
+      code_a += 659
+    end
+
+    printf("%d %d %d %d %d %d %d %d %d %d\n",
+           code_a, code_b, code_c, code_d, code_e,
+           code_f, code_g, code_h, code_i, code_j);
+
+  end
+
+  ## TODO translate the CRC code on page 24
+  def crc(data, len=102)
+    return 4;
   end
 end
